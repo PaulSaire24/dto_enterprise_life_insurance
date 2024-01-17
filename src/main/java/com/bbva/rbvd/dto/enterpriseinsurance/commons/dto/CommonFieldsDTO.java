6b666050cd3c2dc15fa7127cc2f614c76e7f8535
Aaron <carlos.carrillo.delgado@bbva.com>
2024-01-17 11:37:10 -0500

    feat: PERAR-26018 PERAR-26247 New parameters in CommonsDTO

--- src/main/java/com/bbva/rbvd/dto/enterpriseinsurance/commons/dto/CommonFieldsDTO.java
+++ src/main/java/com/bbva/rbvd/dto/enterpriseinsurance/commons/dto/CommonFieldsDTO.java
@@ -19,6 +19,13 @@ public class CommonFieldsDTO implements Serializable {
     private DescriptionDTO period;
     private Boolean isSelected;
     private Boolean isRecommended;
+    private String traceId;
+    private String saleChannelId;
+    private String creationUser;
+    private String userAudit;
+    private String aap;
+    private String externalQuotationId;
+
 
     public String getId() {
         return id;
@@ -90,5 +97,53 @@ public class CommonFieldsDTO implements Serializable {
 
     public void setRecommended(Boolean recommended) {
         isRecommended = recommended;
+    }
+
+    public String getTraceId() {
+        return traceId;
+    }
+
+    public void setTraceId(String traceId) {
+        this.traceId = traceId;
+    }
+
+    public String getSaleChannelId() {
+        return saleChannelId;
+    }
+
+    public void setSaleChannelId(String saleChannelId) {
+        this.saleChannelId = saleChannelId;
+    }
+
+    public String getCreationUser() {
+        return creationUser;
+    }
+
+    public void setCreationUser(String creationUser) {
+        this.creationUser = creationUser;
+    }
+
+    public String getUserAudit() {
+        return userAudit;
+    }
+
+    public void setUserAudit(String userAudit) {
+        this.userAudit = userAudit;
+    }
+
+    public String getAap() {
+        return aap;
+    }
+
+    public void setAap(String aap) {
+        this.aap = aap;
+    }
+
+    public String getExternalQuotationId() {
+        return externalQuotationId;
+    }
+
+    public void setExternalQuotationId(String externalQuotationId) {
+        this.externalQuotationId = externalQuotationId;
     }
 }
