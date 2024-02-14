6d98c221df24d3fefda2e30b20b8f5635be5e7fa
P041360 <marco.yovera@bbva.com>
2024-02-14 11:48:28 -0500

    feat:PERAR-26592 PERAR-26248 Addition of DAO for DB Objects Mapping and constants modification

--- src/main/java/com/bbva/rbvd/dto/enterpriseinsurance/modifyquotation/dao/CommonsDAO.java
+++ src/main/java/com/bbva/rbvd/dto/enterpriseinsurance/modifyquotation/dao/CommonsDAO.java
@@ -10,6 +10,51 @@ public class CommonsDAO {
     private BigDecimal insuranceSimulationId;
     private String customerId;
 
+    public String getInsuranceCompanySimulationId() {
+        return insuranceCompanySimulationId;
+    }
 
+    public void setInsuranceCompanySimulationId(String insuranceCompanySimulationId) {
+        this.insuranceCompanySimulationId = insuranceCompanySimulationId;
+    }
 
+    public BigDecimal getInsuranceProductId() {
+        return insuranceProductId;
+    }
+
+    public void setInsuranceProductId(BigDecimal insuranceProductId) {
+        this.insuranceProductId = insuranceProductId;
+    }
+
+    public String getInsuranceProductType() {
+        return insuranceProductType;
+    }
+
+    public void setInsuranceProductType(String insuranceProductType) {
+        this.insuranceProductType = insuranceProductType;
+    }
+
+    public String getInsuranceBusinessName() {
+        return insuranceBusinessName;
+    }
+
+    public void setInsuranceBusinessName(String insuranceBusinessName) {
+        this.insuranceBusinessName = insuranceBusinessName;
+    }
+
+    public BigDecimal getInsuranceSimulationId() {
+        return insuranceSimulationId;
+    }
+
+    public void setInsuranceSimulationId(BigDecimal insuranceSimulationId) {
+        this.insuranceSimulationId = insuranceSimulationId;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
 }
