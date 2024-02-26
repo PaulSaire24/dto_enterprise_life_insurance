a06b7b979486b89b9a887433f065e0526cf3e1e0
P041360 <marco.yovera@bbva.com>
2024-02-26 11:07:51 -0500

    feat:PERAR-26250 PERAR-26795 Addition of query to get CompanyQuotaId by Internal quotationId

--- src/main/java/com/bbva/rbvd/dto/enterpriseinsurance/utils/RBVDErrors.java
+++ src/main/java/com/bbva/rbvd/dto/enterpriseinsurance/utils/RBVDErrors.java
@@ -3,11 +3,12 @@ package com.bbva.rbvd.dto.enterpriseinsurance.utils;
 public enum RBVDErrors {
     ERROR_PRODUCT_BY_SIMULATION("RBVD00000129", false, "No se encontró información de la cotización"),
     ERROR_SIMULATION_BY_ID("RBVD00000129", false, "No se encontró información de la cotización previa"),
+    ERROR_COMPANY_ID_BY_QUOTATION_ID("RBVD00000129", false, "No se encontró información de la cotizacion externa"),
     ERROR_RIMAC_RESPONSE_SELECT_PLAN("RBVD01020035",false, "No se obtuvo información del plan seleccionado"),
     ERROR_MODALITIES_FROM_DB_BY_PRODUCT("RBVD01020023",false, "Los planes enviados no son los correctos"),
     ERROR_QUOTATION_SAVING("RBVD01020029",false, "No se pudo registrar la cotización en la tabla [PISD.T_PISD_INSURANCE_QUOTATION]"),
     ERROR_QUOTATION_MOD_SAVING("RBVD01020030",false, "No se pudo registrar la cotización en la tabla [PISD.T_PISD_INSURANCE_QUOTATION_MOD]"),
-    ERROR_CALLING_SELECT_PLAN("RBVD01020030",false, "Error al Llamar al Servicio de Seleccionar Plan de Rimac"),
+    ERROR_CALLING_SELECT_PLAN("RBVD01020031",false, "Error al Llamar al Servicio de Seleccionar Plan de Rimac"),
     ERROR_TIMEOUT_RESPONSE("RBVD01020044", false,"Se excedió en el tiempo de espera del servicio de Seleccionar Plan de Rimac");
 
     private final String adviceCode;
