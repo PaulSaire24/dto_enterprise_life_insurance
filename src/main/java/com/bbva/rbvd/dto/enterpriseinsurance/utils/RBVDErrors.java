3b739078ba14d471508654a164037df02c5d0c75
P041360 <marco.yovera@bbva.com>
2024-02-26 11:30:54 -0500

    feat:PERAR-26250 PERAR-26795 Addition of query to get CompanyQuotaId by Internal quotationId

--- src/main/java/com/bbva/rbvd/dto/enterpriseinsurance/utils/RBVDErrors.java
+++ src/main/java/com/bbva/rbvd/dto/enterpriseinsurance/utils/RBVDErrors.java
@@ -3,7 +3,7 @@ package com.bbva.rbvd.dto.enterpriseinsurance.utils;
 public enum RBVDErrors {
     ERROR_PRODUCT_BY_SIMULATION("RBVD00000129", false, "No se encontró información de la cotización"),
     ERROR_SIMULATION_BY_ID("RBVD00000129", false, "No se encontró información de la cotización previa"),
-    ERROR_COMPANY_ID_BY_QUOTATION_ID("RBVD00000129", false, "No se encontró información de la cotizacion externa"),
+    ERROR_COMPANY_ID_BY_QUOTATION_ID("RBVD01020027", false, "No se encontró información del Id de cotizacion de Rimac"),
     ERROR_RIMAC_RESPONSE_SELECT_PLAN("RBVD01020035",false, "No se obtuvo información del plan seleccionado"),
     ERROR_MODALITIES_FROM_DB_BY_PRODUCT("RBVD01020023",false, "Los planes enviados no son los correctos"),
     ERROR_QUOTATION_SAVING("RBVD01020029",false, "No se pudo registrar la cotización en la tabla [PISD.T_PISD_INSURANCE_QUOTATION]"),
